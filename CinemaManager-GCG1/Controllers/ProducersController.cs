83cf7f6052875301ff31c2295e3b6942f410e8c3
firas hamila <firas.hm.1996@gmail.com>
2023-11-09 09:00:49 +0100

    Ajoutez des fichiers projet.

---
+++ CinemaManager-GCG1/Controllers/ProducersController.cs
@@ -0,0 +1,89 @@
+﻿using CinemaManager_GCG1.Models.Cinema;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaManager_GCG1.Controllers
+{
+    public class ProducersController : Controller
+    {
+        CinemaDbGcg1Context _context;
+        public ProducersController(CinemaDbGcg1Context context)
+        {
+            _context = context;
+        }
+        // GET: ProducersController
+        public ActionResult Index()
+        {
+            return View(_context.Producers.ToList());
+        }
+
+        // GET: ProducersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProducersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CinemaManager-GCG1/Models/Cinema/CinemaDbGcg1Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaManager_GCG1.Models.Cinema;
+
+public partial class CinemaDbGcg1Context : DbContext
+{
+    public CinemaDbGcg1Context()
+    {
+    }
+
+    public CinemaDbGcg1Context(DbContextOptions<CinemaDbGcg1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producer> Producers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:CinemaCS");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producer__3214EC0767A9B3A8");
+
+            entity.ToTable("Producer");
+
+            entity.Property(e => e.Email).HasMaxLength(30);
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.Nationality).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
